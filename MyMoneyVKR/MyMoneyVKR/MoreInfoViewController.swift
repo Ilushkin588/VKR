58d1b9a4c83b5d744c15adadc2978d98440095d0
Ilushkin <denisow.ilya2009@yandex.ru>
2017-04-27 01:41:40 +0300

    Fifth commit

--- MyMoneyVKR/MyMoneyVKR/MoreInfoViewController.swift
+++ MyMoneyVKR/MyMoneyVKR/MoreInfoViewController.swift
@@ -19,6 +19,87 @@ struct cellData {
     let emojiImage : UIImage!
 }
 
+func fillTheCellData() -> [cellData]{
+    let realmMoney = try! Realm()
+    let allPlusMoney = realmMoney.objects(PlusMoney.self)
+    let allMinusMoney = realmMoney.objects(MinusMoney.self)
+    var imageType : UIImage!
+    
+    var arrayOfCells = [cellData]()
+    
+    var emojiImage = UIImage()
+    for money in allPlusMoney {
+        if (money.type == "Карта"){
+            imageType = UIImage(named:"cardPlus")
+        } else {
+            imageType = UIImage(named:"cashPlus")
+        }
+        
+        switch money.category {
+        case "Зарплата" :
+            emojiImage = UIImage(named: "salaryImage")!
+        case "Бизнес" :
+            emojiImage = UIImage(named: "businessImage")!
+        case "Долги" :
+            emojiImage = UIImage(named: "debtsImage")!
+        case "Подарок" :
+            emojiImage = UIImage(named: "presentImage")!
+        case "Проценты" :
+            emojiImage = UIImage(named: "bankAccountImage")!
+        case "Сбережения" :
+            emojiImage = UIImage(named: "savingsImage")!
+        default:
+            break
+            
+        }
+        let newElem = cellData.init(name: money.name, amount: "+" + String(money.amount) + "руб", date: Date().theFormattedDate(date: money.createdAT), time: Date().theFormattedTime(time: money.createdAT), imageType: imageType, emojiImage: emojiImage)
+        arrayOfCells.append(newElem)
+    }
+    for money in allMinusMoney {
+        if (money.type == "Карта"){
+            imageType = UIImage(named:"cardMinus")
+        } else {
+            imageType = UIImage(named:"cashMinus")
+        }
+        
+        switch money.category {
+        case "Машина" :
+            emojiImage = UIImage(named: "carImage")!
+        case "Одежда" :
+            emojiImage = UIImage(named: "clothesImage")!
+        case "Еда" :
+            emojiImage = UIImage(named: "foodImage")!
+        case "Развлечения" :
+            emojiImage = UIImage(named: "entertainmentImage")!
+        case "Счета" :
+            emojiImage = UIImage(named: "houseBillsImage")!
+        case "Гигиена" :
+            emojiImage = UIImage(named: "hygieneImage")!
+        case "Медицина" :
+            emojiImage = UIImage(named: "madicineImage")!
+        case "Животные" :
+            emojiImage = UIImage(named: "petsImage")!
+        case "Магазины" :
+            emojiImage = UIImage(named: "shopsImage")!
+        case "Такси" :
+            emojiImage = UIImage(named: "taxiImage")!
+        case "Транспорт" :
+            emojiImage = UIImage(named: "transportImage")!
+        case "Спорт" :
+            emojiImage = UIImage(named: "sportsImage")!
+        default:
+            break
+        }
+        
+        let newElem = cellData.init(name: money.name, amount: "-" + String(money.amount) + "руб", date: Date().theFormattedDate (date: money.createdAT), time: Date().theFormattedTime(time: money.createdAT), imageType: imageType, emojiImage : emojiImage)
+        arrayOfCells.append(newElem)
+    }
+    return arrayOfCells
+}
+
+    let array = fillTheCellData()
+    var filteredArray = fillTheCellData()
+    //filteredArray = filteredArray.sorted() {$0.time > $1.time}
 extension Date {
     
     // formatted date
@@ -55,87 +136,9 @@ class MoreInfoViewController: UIViewController, UITableViewDelegate, UITableView
     
     @IBOutlet weak var calendarView: JTAppleCalendarView!
     @IBOutlet weak var historyTableView: UITableView!
-    func fillTheCellData() -> [cellData]{
-        let realmMoney = try! Realm()
-        let allPlusMoney = realmMoney.objects(PlusMoney.self)
-        let allMinusMoney = realmMoney.objects(MinusMoney.self)
-        var imageType : UIImage!
-        
-        var arrayOfCells = [cellData]()
-
-        var emojiImage = UIImage()
-        for money in allPlusMoney {
-            if (money.type == "Карта"){
-                imageType = UIImage(named:"cardPlus")
-            } else {
-                imageType = UIImage(named:"cashPlus")
-            }
-            
-            switch money.category {
-            case "Зарплата" :
-                emojiImage = UIImage(named: "salaryImage")!
-            case "Бизнес" :
-                emojiImage = UIImage(named: "businessImage")!
-            case "Долги" :
-                emojiImage = UIImage(named: "debtsImage")!
-            case "Подарок" :
-                emojiImage = UIImage(named: "presentImage")!
-            case "Проценты" :
-                emojiImage = UIImage(named: "bankAccountImage")!
-            case "Сбережения" :
-                emojiImage = UIImage(named: "savingsImage")!
-            default:
-                break
-                
-            }
-            let newElem = cellData.init(name: money.name, amount: "+" + String(money.amount) + "руб", date: Date().theFormattedDate(date: money.createdAT), time: Date().theFormattedTime(time: money.createdAT), imageType: imageType, emojiImage: emojiImage)
-            arrayOfCells.append(newElem)
-        }
-        for money in allMinusMoney {
-            if (money.type == "Карта"){
-                imageType = UIImage(named:"cardMinus")
-            } else {
-                imageType = UIImage(named:"cashMinus")
-            }
-            
-            switch money.category {
-            case "Машина" :
-                emojiImage = UIImage(named: "carImage")!
-            case "Одежда" :
-                emojiImage = UIImage(named: "clothesImage")!
-            case "Еда" :
-                emojiImage = UIImage(named: "foodImage")!
-            case "Развлечения" :
-                emojiImage = UIImage(named: "entertainmentImage")!
-            case "Счета" :
-                emojiImage = UIImage(named: "houseBillsImage")!
-            case "Гигиена" :
-                emojiImage = UIImage(named: "hygieneImage")!
-            case "Медицина" :
-                emojiImage = UIImage(named: "madicineImage")!
-            case "Животные" :
-                emojiImage = UIImage(named: "petsImage")!
-            case "Магазины" :
-                emojiImage = UIImage(named: "shopsImage")!
-            case "Такси" :
-                emojiImage = UIImage(named: "taxiImage")!
-            case "Транспорт" :
-                emojiImage = UIImage(named: "transportImage")!
-            case "Спорт" :
-                emojiImage = UIImage(named: "sportsImage")!
-            default:
-                break
-            }
-            
-            let newElem = cellData.init(name: money.name, amount: "-" + String(money.amount) + "руб", date: Date().theFormattedDate (date: money.createdAT), time: Date().theFormattedTime(time: money.createdAT), imageType: imageType, emojiImage : emojiImage)
-            arrayOfCells.append(newElem)
-        }
-        return arrayOfCells
-    }
-
+    
     
     func handleCellTextColor(view: JTAppleDayCellView?, cellState: CellState) {
-        let array = fillTheCellData()
         
         guard let calendarCell = view as? CalendarCell else {
             return
@@ -143,16 +146,13 @@ class MoreInfoViewController: UIViewController, UITableViewDelegate, UITableView
         
         if cellState.isSelected {
             calendarCell.dayLabel.textColor = white
-            var i = 0
-            for data in array {
-                if (Date().theFormattedDate(date: cellState.date as NSDate) == data.date){
-                    
+             filteredArray = array.filter { $0.date == Date().theFormattedDate(date: cellState.date as NSDate)  }
+            if filteredArray.isEmpty {
+                historyTableView.isHidden = true
+            }
+            for _ in filteredArray {
+                    historyTableView.reloadData()
                     historyTableView.isHidden = false
-                }
-                else {
-                    historyTableView.isHidden = true
-                }
-                i += 1
             }
         } else {
             
@@ -211,7 +211,7 @@ class MoreInfoViewController: UIViewController, UITableViewDelegate, UITableView
         historyTableView.register(UINib(nibName: "MoreInfoTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
         calendarView.dataSource = self
         calendarView.delegate = self
-        calendarView.registerCellViewXib(file: "CalendarCell") // Registering your cell is manditory
+        calendarView.registerCellViewXib(file: "CalendarCell") // Registering cell is manditory
     }
 
     
@@ -220,24 +220,23 @@ class MoreInfoViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let array = fillTheCellData()
-        return array.count
+        return filteredArray.count
     }
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        filteredArray = filteredArray.sorted() {$0.time > $1.time}
+        filteredArray = filteredArray.sorted() {$0.date > $1.date}
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MoreInfoTableViewCell
-       let array = fillTheCellData()
-        if (array[indexPath.row].amount.characters.contains("+")){
+        if (filteredArray[indexPath.row].amount.characters.contains("+")){
             cell.amountLabel.textColor = UIColor.init(red: 129.0/255.0, green: 184/255.0, blue: 12/255.0, alpha: 1.0)
         } else {
              cell.amountLabel.textColor = UIColor(red: 234.0/255.0, green: 78.0/255.0, blue: 51.0/255.0, alpha: 1.0)
         }
-        cell.amountLabel.text = array[indexPath.row].amount
-        cell.nameLabel.text = array[indexPath.row].name
-        cell.dateAndDayLabel.text = array[indexPath.row].date
-        cell.timeLabel.text = array[indexPath.row].time
-        cell.cardOrCashImageView.image = array[indexPath.row].imageType
-        cell.emojiImageView.image = array[indexPath.row].emojiImage
+        cell.amountLabel.text = filteredArray[indexPath.row].amount
+        cell.nameLabel.text = filteredArray[indexPath.row].name
+        cell.dateAndDayLabel.text = filteredArray[indexPath.row].date
+        cell.timeLabel.text = filteredArray[indexPath.row].time
+        cell.cardOrCashImageView.image = filteredArray[indexPath.row].imageType
+        cell.emojiImageView.image = filteredArray[indexPath.row].emojiImage
         
         cell.layer.borderWidth = 2.0
         cell.layer.borderColor = UIColor.gray.cgColor
